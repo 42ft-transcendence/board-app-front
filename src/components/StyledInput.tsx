747f2cf41f64ed9b8234789fa88bb39216240fe3
42sangkkim <sangkkim@student.42seoul.kr>
2023-07-21 00:15:17 +0900

    Feat: set input text color

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -2,10 +2,11 @@ import { styled } from "styled-components";
 
 
 export const StyledInput = styled.input`
-  width: 300px
-  height: 40px
-  border-radius: 20px
-  background-color: #A4A6AF
-  text-align: center
-  border: None
+  width: 300px;
+  height: 40px;
+  border: None;
+  border-radius: 20px;
+  background-color: #A4A6AF;
+  text-align: center;
+  color: #E5EAF5;
 `;
