b300b172d31d53f14bf9ce95fb5f5d223af0ab67
42sangkkim <sangkkim@student.42seoul.kr>
2023-07-21 00:01:25 +0900

    Feat: create styled input compoenent

---
+++ src/components/StyledInput.tsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+
+export const StyledInput = styled.input`
+  width: 300px
+  height: 40px
+  border-radius: 20px
+  background-color: #A4A6AF
+  text-align: center
+  border: None
+`;
