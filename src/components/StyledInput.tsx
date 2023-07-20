cbbb1694e708d7c0207c895640dacb5cbcff1052
42sangkkim <sangkkim@student.42seoul.kr>
2023-07-21 00:39:35 +0900

    Style: edit design

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -4,9 +4,10 @@ import { styled } from "styled-components";
 export const StyledInput = styled.input`
   width: 300px;
   height: 40px;
-  border: None;
+  border: none;
   border-radius: 20px;
   background-color: #A4A6AF;
   text-align: center;
   color: #E5EAF5;
+  margin-block: 10px;
 `;
